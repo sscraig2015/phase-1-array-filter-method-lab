b62bdd84013a47dbc58cbb7d003bd075be55cc15
Sean <craig.sean92@gmail.com>
2021-10-24 18:40:53 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, nameFiltered) {
+    let matchingNames = array.filter(name => name.toUpperCase() === nameFiltered.toUpperCase())
+    return matchingNames
+}
+
+function fuzzyMatch(array, startLet) {
+    let matchingFirst = array.filter( name => name.startsWith(startLet)) 
+    return matchingFirst
+}
+
+function matchName(array, inputName) {
+    let matchingName = array.filter( array=> array.name === inputName)
+    return matchingName
+}
+
